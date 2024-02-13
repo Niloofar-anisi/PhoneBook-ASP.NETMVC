802bc56d2e8f1a59bdec2c4a3c994df8be0700d9
NiloofarAnisi <n.anisi@yahoo.com>
2024-02-13 20:40:15 +0330

    Add project files.

---
+++ PhoneBookMVC/Controllers/TblUsersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PhoneBookMVC.Models.DominModels;
+
+namespace PhoneBookMVC.Controllers
+{
+    public class TblUsersController : Controller
+    {
+        private phoneBookEntities db = new phoneBookEntities();
+
+        // GET: TblUsers
+        public ActionResult Index()
+        {
+            return View(db.TblUsers.ToList());
+        }
+
+        // GET: TblUsers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TblUser tblUser = db.TblUsers.Find(id);
+            if (tblUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUser);
+        }
+
+        // GET: TblUsers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TblUsers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Family,Mobile,PhoneNumber,NationalCode,OrgPos")] TblUser tblUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TblUsers.Add(tblUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblUser);
+        }
+
+        // GET: TblUsers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TblUser tblUser = db.TblUsers.Find(id);
+            if (tblUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUser);
+        }
+
+        // POST: TblUsers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Family,Mobile,PhoneNumber,NationalCode,OrgPos")] TblUser tblUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblUser);
+        }
+
+        // GET: TblUsers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TblUser tblUser = db.TblUsers.Find(id);
+            if (tblUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUser);
+        }
+
+        // POST: TblUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TblUser tblUser = db.TblUsers.Find(id);
+            db.TblUsers.Remove(tblUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PhoneBookMVC/Models/EntityModels/TblUserMetaData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBookMVC.Models.EntityModels
+{
+    public class TblUserMetaData
+    {
+        
+        public int ID { get; set; }
+        [Display(Name="نام")]
+        [Required(ErrorMessage ="نام را وارد کنید")]
+        
+        public string Name { get; set; }
+        [Display(Name = "نام خانوادگی")]
+        [Required(ErrorMessage = "نام خانوادگی را وارد کنید")]
+        public string Family { get; set; }
+        [Display(Name = "موبایل")]
+        [Required(ErrorMessage = "موبایل را وارد کنید")]
+        [RegularExpression(@"^0\d{10}$", ErrorMessage = "لطفا یک شماره موبایل 11 رقمی که با صفر شروع شود وارد کنید")]
+        [Range(0, 99999999999, ErrorMessage = "لطفا یک شماره موبایل 11 رقمی که با صفر شروع شود وارد کنید")]
+        public string Mobile { get; set; }
+        [Display(Name = "تلفن ثابت")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "کد ملی")]
+        [Required(ErrorMessage = "کد ملی را وارد کنید")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "لطفا یک کد ملی 10 رقمی وارد کنید")]
+        [StringLength(10, ErrorMessage = "کد ملی باید 10 رقم باشد")]
+        public string NationalCode { get; set; }
+        [Display(Name = "شغل")]
+        public string OrgPos { get; set; }
+    }
+}
+
+namespace PhoneBookMVC.Models.DominModels
+{
+    [MetadataType(typeof(EntityModels.TblUserMetaData))]
+    public partial class TblUser
+    {
+
+    }
+}
